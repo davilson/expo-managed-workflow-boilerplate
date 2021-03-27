95311cfb552a529e09f12e775be12d0c9a6eb1b6
Davilson de Castro <davilsondecastro@gmail.com>
2021-03-27 02:16:03 -0300

    feat: adiciona configuração de reactnavigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-
-import Main from './src/components/pages/Main';
+import Routes from './src/routes';
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <Main />
+      <Routes />
     </>
   );
 }
---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Main from '../components/pages/Main';
+
+const Stack = createStackNavigator();
+
+const MyStack = () => (
+  <Stack.Navigator initialRouteName="Main">
+    <Stack.Screen name="Main" component={Main} />
+  </Stack.Navigator>
+);
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
